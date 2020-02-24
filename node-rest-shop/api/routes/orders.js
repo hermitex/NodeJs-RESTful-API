5b2287d8b48bf2b4a7a56f02d2b33a6321fc4e29
hermitex <mercytiberius@yahoo.com>
2020-02-24 20:44:17 +0300

    fixed orders routes

--- node-rest-shop/api/routes/orders.js
+++ node-rest-shop/api/routes/orders.js
@@ -6,10 +6,17 @@ const Order = require("../models/order");
 
 router.get("/", (request, response, next) => {
     Order.find().exec().then(docs => {
-        console.log(docs);
-        response.status(200).json(
-            docs
-        )
+        if (docs.length > 0) {
+            console.log(docs);
+            response.status(200).json(
+                docs
+            )
+        } else {
+            response.status(404).json({
+                message: "No entries found"
+            })
+        }
+
     })
         .catch(error => {
             console.log(error);
@@ -42,19 +49,51 @@ router.post("/", (request, response, next) => {
 })
 
 router.get("/:orderId", (request, response, next) => {
-    response.status(200).json({
-        message: "Orders details",
-        orderId: request.params.productId,
+    const id = request.params.orderId;
+    Order.findById(id).exec().then(doc => {
+        if (doc) {
+            console.log(doc);
+            response.status(200).json({
+                order: doc
+            })
+        } else {
+            response.status(404).json({
+                message: "No entries",
+            })
+        }
+
+    }).catch(error => {
+        console.log(error)
+        response.status(500).json({
+            error
+        })
     })
+
 })
 
 
 
 router.delete("/:orderId", (request, response, next) => {
-    response.status(200).json({
-        message: "Orders deleted",
-        orderId: request.params.productId,
+    const id = request.params.orderId;
+
+    Order.remove({ _id: id }).exec().then(doc => {
+
+        console.log(doc)
+        response.status(200).json({
+            message: "Orders deleted",
+            orderId: id,
+        })
+
+        response.status(404).json({
+            message: "No entries found"
+        })
+
+    }).catch(error => {
+        response.status(500).json({
+            error
+        })
     })
+
 })
 
 
